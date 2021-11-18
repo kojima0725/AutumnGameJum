966ccab5835ca4767eff50c324309485f8603227
kojima0725 <kojikoji0725kt@gmail.com>
2021-11-18 16:47:20 +0900

    GameTimer

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField]
+    float timer = 30.0f;
+
+    [SerializeField]
+    GameObject resultButton;
+
+    [SerializeField]
+    GameObject currentScore;
+
+    [SerializeField]
+    Text ui;
+
+    [SerializeField]
+    Text result;
+
+
+
+    bool ingame;
+    public void StartGame()
+    {
+        ingame = true;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            ingame = false;
+            resultButton.SetActive(true);
+            currentScore.SetActive(false);
+            result.text = GameManager.Instance.Score.ToString();
+        }
+        else
+        {
+            ui.text = ((int)timer).ToString();
+        }
+    }
+
+
+}
