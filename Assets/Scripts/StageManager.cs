cca18de4ed118ae5af52bf5fc53626d074d34b0d
kojima0725 <kojikoji0725kt@gmail.com>
2021-11-18 13:25:07 +0900

    ゲーム開始機能

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameManager.Instance.GameStart();
     }
 }
