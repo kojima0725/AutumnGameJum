abcf475d56674a307d9c4f3f5ea5bef36d9c06a5
MutoAyumu <mutoayum0610@gmail.com>
2021-11-18 15:44:27 +0900

    調整

--- Assets/Muto/PlayerMoveController.cs
+++ Assets/Muto/PlayerMoveController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMoveController : MonoBehaviour
 {
     [SerializeField] float _moveSpeed = 2f;
