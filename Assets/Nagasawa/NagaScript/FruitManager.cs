188157950b6e257ed0167955a211627c8fd59515
NagasawaYusei <curga3013@gmail.com>
2021-11-18 15:04:17 +0900

    FruitManager

---
+++ Assets/Nagasawa/NagaScript/FruitManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitManager : MonoBehaviour
+{
+    public int m_point;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            Destroy(this);
+        }
+    }
+}
