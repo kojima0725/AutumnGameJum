5175142cc912bdb5b8d6dc4f85d2a5e82e2e62c8
kojima0725 <kojikoji0725kt@gmail.com>
2021-11-18 13:23:13 +0900

    スコア加算機能

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,21 @@ public class GameManager : MonoBehaviour
     static GameManager _instance;
     public static GameManager Instance => _instance;
 
+    int _score;
+    public int Score => _score;
+
     void Awake()
     {
         _instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void GameStart()
+    {
+        _score = 0;
+    }
+    public void GetItem(int score)
+    {
+        _score += score;
     }
 }
